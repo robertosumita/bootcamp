a1885fac285ae0797f60445a26b3b1ea706315f8
upm6 <upm6@U-29C9TZKSNNGHL.corp.amazonworkspaces.com>
2024-10-16 13:48:01 +0000

    Workflows version 1

---
+++ ice-cream-dataops/function_local_venvs/oee_timeseries/run_check.py
@@ -0,0 +1,38 @@
+import os
+from pprint import pprint
+
+from cognite.client import CogniteClient, ClientConfig
+from cognite.client.credentials import OAuthClientCredentials
+
+from code.handler import handle
+
+
+def main() -> None:
+    credentials = OAuthClientCredentials(
+        token_url="https://login.microsoftonline.com/16e3985b-ebe8-4e24-9da4-933e21a9fc81/oauth2/v2.0/token",
+        client_id="23f3d8d9-e0fc-4291-8881-bf401134af9c",
+        client_secret=os.environ["IDP_CLIENT_SECRET"],
+        scopes=['https://westeurope-1.cognitedata.com/.default'],
+    )
+
+    client = CogniteClient(
+        config=ClientConfig(
+            client_name="CDF-Toolkit:0.3.0b4",
+            project="cdf-bootcamp-31-test",
+            base_url="https://westeurope-1.cognitedata.com",
+            credentials=credentials,
+        )
+    )
+
+    print("oee_timeseries LOGS:")
+    response = handle(
+        client=client,
+        data={},
+    )
+
+    print("oee_timeseries RESPONSE:")
+    pprint(response)
+
+
+if __name__ == "__main__":
+    main()
